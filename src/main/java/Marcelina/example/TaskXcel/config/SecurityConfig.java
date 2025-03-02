a365e37569bee5d6e2d4d4eb76a20c0f7c57932a
marcipeto <marcelina.peto09@gmail.com>
2025-03-02 13:20:23 +0000

    commit

--- src/main/java/Marcelina/example/TaskXcel/config/SecurityConfig.java
+++ src/main/java/Marcelina/example/TaskXcel/config/SecurityConfig.java
@@ -20,6 +20,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
+
                         .anyRequest().permitAll()
                 )
                 .csrf(AbstractHttpConfigurer::disable)
